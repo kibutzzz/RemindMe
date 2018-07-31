38993fdddc9e08985ddc7cbb507a7cde9138bed7
leonardo <leonardo.meinerzramos28@gmail.com>
2018-07-31 11:50:01 -0300

    removido um import não usado

--- app/src/main/java/com/ifsul/remindme/MainActivity.java
+++ app/src/main/java/com/ifsul/remindme/MainActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
-import sun.net.ResourceManager;
+
 
 public class MainActivity extends AppCompatActivity {
 

b637edf767a1179e4ef3ca87ec071810fb8a0fc7
leonardo <leonardo.meinerzramos282gmail.com>
2018-08-05 18:39:16 -0300

    adicionado a função de cadastrar no firebase

--- app/src/main/java/com/ifsul/remindme/AddTaskActivity.java
+++ app/src/main/java/com/ifsul/remindme/AddTaskActivity.java
@@ -2,12 +2,51 @@ package com.ifsul.remindme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatEditText;
+import android.view.View;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AddTaskActivity extends AppCompatActivity {
+
+    private static AppCompatEditText descricaoEditText;
+    private static AppCompatEditText nomeEditText;
+    private static AppCompatButton registerButton;
+
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference tasksDatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+        descricaoEditText = findViewById(R.id.edit_text_desc);
+        nomeEditText = findViewById(R.id.edit_text_nome);
+        registerButton = findViewById(R.id.button_register);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        tasksDatabaseReference = firebaseDatabase.getReference("tasks");
+
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Task t = new Task(
+                        nomeEditText.getText().toString(),
+                        descricaoEditText.getText().toString());
+
+                tasksDatabaseReference.push().setValue(t);
+                clearFields();
+
+            }
+        });
+    }
+
+    private void clearFields(){
+        nomeEditText.getText().clear();
+        descricaoEditText.getText().clear();
     }
 }

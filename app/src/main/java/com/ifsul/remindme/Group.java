5e8cc7221690302822f70cc2fa9a6a8f3a1c1182
Leonardo Meinerz Ramos <leonardo.meinerzramos28@gmail.com>
2018-08-18 18:02:29 -0300

    adicionados getters and setters e o atributo participantes

--- app/src/main/java/com/ifsul/remindme/Group.java
+++ app/src/main/java/com/ifsul/remindme/Group.java
@@ -1,20 +1,39 @@
 package com.ifsul.remindme;
 
+import java.util.List;
+
 public class Group {
     private String nome;
     private String descricao;
+    private List<String> participates;
+
+    Group(String nome, String descricao, List<String> participates) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.participates = participates;
+    }
 
     public String getNome() {
         return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
     public String getDescricao() {
         return descricao;
     }
 
-    public Group(String nome, String descricao) {
-
-        this.nome = nome;
+    public void setDescricao(String descricao) {
         this.descricao = descricao;
+    }
+
+    public List<String> getParticipates() {
+        return participates;
+    }
+
+    public void setParticipates(List<String> participates) {
+        this.participates = participates;
     }
 }

44d1e82fa220a767418288af658c917477436dbe
Leonardo Meinerz Ramos <leonardo.meinerzramos28@gmail.com>
2018-09-02 22:29:41 -0300

    adicionado o campo da key na viewholder e adicionado um click listener para acessar a tela de detalhes

--- app/src/main/java/com/ifsul/remindme/TaskViewHolder.java
+++ app/src/main/java/com/ifsul/remindme/TaskViewHolder.java
@@ -1,19 +1,35 @@
 package com.ifsul.remindme;
 
+import android.content.Intent;
+import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
+import com.ifsul.remindme.activities.TaskDetailActivity;
+
 public class TaskViewHolder extends RecyclerView.ViewHolder {
+
     public final TextView nome;
     public final TextView descricao;
     public final TextView limite;
+    public final AppCompatTextView key;
 
-    public TaskViewHolder(View view){
+    public TaskViewHolder(View view) {
         super(view);
         nome = view.findViewById(R.id.text_view_nome);
         descricao = view.findViewById(R.id.text_view_descricao);
         limite = view.findViewById(R.id.text_view_date);
+        key = view.findViewById(R.id.task_id);
 
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), TaskDetailActivity.class);
+                i.putExtra("key", key.getText().toString());
+                view.getContext().startActivity(i);
+            }
+        });
     }
 }

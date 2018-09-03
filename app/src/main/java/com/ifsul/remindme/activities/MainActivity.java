92d5f6df332766559e8b1a0de00ae3bec2e09ee9
Leonardo Meinerz Ramos <leonardo.meinerzramos28@gmail.com>
2018-09-03 13:51:57 -0300

    Implementação parcial de do fluxo de login FirebaseUI

--- app/src/main/java/com/ifsul/remindme/activities/MainActivity.java
+++ app/src/main/java/com/ifsul/remindme/activities/MainActivity.java
@@ -1,21 +1,26 @@
 package com.ifsul.remindme.activities;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabItem;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-import com.ifsul.remindme.adapters.PageAdapter;
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
 import com.ifsul.remindme.R;
+import com.ifsul.remindme.adapters.PageAdapter;
+
+import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int RC_SIGN_IN = 123;
     Toolbar toolbar;
     TabLayout tabLayout;
     ViewPager viewPager;
@@ -23,16 +28,35 @@ public class MainActivity extends AppCompatActivity {
     TabItem tabTarefas;
     TabItem tabGrupos;
     FloatingActionButton floatingActionButton;
+    FirebaseAuth firebaseAuth;
+    FirebaseAuth.AuthStateListener authStateListener;
+    String user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() != null){
+            //logado
+        } else{
+            //deslogado
+            startActivityForResult(
+                    // Get an instance of AuthUI based on the default app
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(Arrays.asList(
+                                    new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                    new AuthUI.IdpConfig.EmailBuilder().build()))
+                            .build(),
+                    RC_SIGN_IN);
+        }
 
         toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle(getResources().getString(R.string.app_name));
         setSupportActionBar(toolbar);
+
 
         tabLayout = findViewById(R.id.tabLayout);
         tabTarefas = findViewById(R.id.tabTarefas);
@@ -67,11 +91,30 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AddTaskActivity.class);
                 startActivity(intent);
-
             }
         });
 
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
+    }
+
+    /**
+     * Dispatch incoming result to the correct fragment.
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //se o request for do login
+        if (requestCode == RC_SIGN_IN){
+
+            //se o login foi concluido com sucesso
+            if(resultCode == RESULT_OK){
+                //TODO setar usuario atual
+
+            }
+        }
     }
 }

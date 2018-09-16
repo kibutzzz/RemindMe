1f33baeae1383476882ed373658bb666343470cd
Leonardo Meinerz Ramos <leonardo.meinerzramos28@gmail.com>
2018-09-15 23:53:41 -0300

    adicionado o datepickerdialog para o usuario escolher a data limite da sua tarefa

--- app/src/main/java/com/ifsul/remindme/activities/AddTaskActivity.java
+++ app/src/main/java/com/ifsul/remindme/activities/AddTaskActivity.java
@@ -1,11 +1,13 @@
 package com.ifsul.remindme.activities;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.AppCompatEditText;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.DatePicker;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -13,12 +15,18 @@ import com.ifsul.remindme.database.DatabaseUtils;
 import com.ifsul.remindme.R;
 import com.ifsul.remindme.Task;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class AddTaskActivity extends AppCompatActivity {
 
     private AppCompatEditText descricaoEditText;
     private AppCompatEditText nomeEditText;
+    private AppCompatEditText limiteEditText;
     private AppCompatButton registerButton;
     private Toolbar toolbar;
+    private GregorianCalendar currentDate;
+    private int day, month, year;
 
     private FirebaseDatabase firebaseDatabase;
     private DatabaseReference tasksDatabaseReference;
@@ -34,20 +42,24 @@ public class AddTaskActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setHomeButtonEnabled(true);
         }
+
         descricaoEditText = findViewById(R.id.edit_text_desc);
         nomeEditText = findViewById(R.id.edit_text_nome);
+        limiteEditText = findViewById(R.id.edit_text_limite);
         registerButton = findViewById(R.id.button_register);
 
         firebaseDatabase = DatabaseUtils.getFirebaseDatabase();
         tasksDatabaseReference = firebaseDatabase.getReference("tasks").child(MainActivity.usuario.getUid());
 
+        setDatePicker();
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Task t = new Task(
                         nomeEditText.getText().toString(),
-                        descricaoEditText.getText().toString());
+                        descricaoEditText.getText().toString(),
+                        day, month, year);
 
                 tasksDatabaseReference.push().setValue(t);
                 finish();
@@ -56,4 +68,30 @@ public class AddTaskActivity extends AppCompatActivity {
     }
 
 
+    private void setDatePicker(){
+
+        //pega a data de amanhã
+        currentDate = new GregorianCalendar();
+        currentDate.add(Calendar.DATE, 1);
+
+        day = currentDate.get(GregorianCalendar.DAY_OF_MONTH);
+        month = currentDate.get(GregorianCalendar.MONTH);
+        year = currentDate.get(GregorianCalendar.YEAR);
+        limiteEditText.setText(day + "/" + (month + 1) + "/" + year);
+
+        limiteEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(AddTaskActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        month = month + 1;
+                        limiteEditText.setText(day + "/" + month + "/" + year);
+                    }
+                }, year, month, day-1);
+                datePickerDialog.show();
+            }
+        });
+    }
 }
